3a1ea974da9eb00b8fccd84a949646230ac3c74d
Matias Masucci <masumatias@gmail.com>
2025-03-18 16:55:50 -0300

    Update Concecionario.java

--- tpo_PDS-main/src/main/java/com/pds/project/Models/Concecionario.java
+++ tpo_PDS-main/src/main/java/com/pds/project/Models/Concecionario.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Concecionario {
    protected String nombre;
    protected Integer cuit; 
-   protected ArrayList<Vehiculo> vehiculos;
+   protected ArrayList<Vehiculo> catalogo;
    protected ArrayList<Vendedor> vendedores;
    protected ArrayList<Comprador> compradores;
    protected ArrayList<Pedido> pedidos;
@@ -12,7 +12,7 @@ public class Concecionario {
    public Concecionario(String nombre, Integer cuit) {
       this.nombre = nombre;
       this.cuit = cuit;
-      vehiculos = new ArrayList<Vehiculo>();
+      catalogo = new ArrayList<Vehiculo>();
       vendedores = new ArrayList<Vendedor>();
       pedidos = new ArrayList<Pedido>();
       compradores = new ArrayList<Comprador>();
