6daaebbdc3ec6e33562f87ad455236ebe3cc208c
Juan Manuel Gardon <gardonjuanmanuel@gmail.com>
2025-03-19 17:23:30 -0300

    add Getter and Setter

--- src/main/java/com/pds/project/Models/Comprador.java
+++ src/main/java/com/pds/project/Models/Comprador.java
@@ -1,6 +1,11 @@
 package com.pds.project.Models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Compradores")
@@ -29,4 +34,60 @@ public class Comprador {
 
     @Column(unique = true, name = "Cuit/Cuil")
     protected Integer cuitCuil;    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(Integer documento) {
+        this.documento = documento;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getCuitCuil() {
+        return cuitCuil;
+    }
+
+    public void setCuitCuil(Integer cuitCuil) {
+        this.cuitCuil = cuitCuil;
+    }
 }
