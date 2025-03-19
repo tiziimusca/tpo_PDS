7adec70776a1b6b471746bca7a2bf9f5658c3977
Juan Manuel Gardon <gardonjuanmanuel@gmail.com>
2025-03-19 17:26:59 -0300

    repositorio comprador

---
+++ src/main/java/com/pds/project/Models/repositorio/repositorioComprador.java
@@ -0,0 +1,9 @@
+package com.pds.project.Models.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pds.project.Models.Comprador;
+
+public interface repositorioComprador extends JpaRepository<Comprador, Long> {
+
+}
