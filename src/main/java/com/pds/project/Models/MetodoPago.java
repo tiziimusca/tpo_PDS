6dc79edaa26d8a3be6343db23a149e7a29fb7fc4
matimasu <masumatias@gmail.com>
2025-03-20 23:41:49 -0300

    correccion apellido a correo

--- src/main/java/com/pds/project/Models/MetodoPago.java
+++ src/main/java/com/pds/project/Models/MetodoPago.java
@@ -12,6 +12,5 @@ import jakarta.persistence.InheritanceType;
 @DiscriminatorColumn(name = "Metodo de pago", discriminatorType = DiscriminatorType.STRING)
 public abstract class MetodoPago {
     public MetodoPago() {}
-
     public abstract double pagar(Vehiculo vehiculo);
 }
